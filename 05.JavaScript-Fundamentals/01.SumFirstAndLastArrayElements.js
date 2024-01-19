1ab563bed67769ff7334f918790695de59fb60b0
Simeon <simeonsavov21@gmail.com>
2024-01-19 09:23:36 +0200

    feat: Add function to add first and last numbers
    Implemented a JavaScript function to add the first and last numbers of an input array. The function extracts the first and last elements of the array and outputs their sum. This task focuses on array indexing and basic arithmetic operations in JavaScript.

---
+++ 05.JavaScript-Fundamentals/01.SumFirstAndLastArrayElements.js
@@ -0,0 +1,8 @@
+function solve(inputArr) {
+    'use strict';
+
+    let firstNumber = inputArr[0];
+    let lastNumber = inputArr[inputArr.length - 1];
+
+    console.log(firstNumber + lastNumber);
+}
