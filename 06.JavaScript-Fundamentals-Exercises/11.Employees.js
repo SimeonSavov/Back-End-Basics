cfc85bb958bf1d26dbc022db823a181c31d80851
Simeon <simeonsavov21@gmail.com>
2024-01-24 17:04:22 +0200

    feat: Implement employee data processing function
    Implemented a JavaScript function to process an array of employee names, generating and displaying corresponding employee data. The function calculates the length of each name and creates an array of objects containing employee names and their corresponding personal numbers.

---
+++ 06.JavaScript-Fundamentals-Exercises/11.Employees.js
@@ -0,0 +1,14 @@
+function solve(arrayOfEmployees) {
+    'use strict';
+
+    const employeeData = [];
+
+    for (const employeeRaw of arrayOfEmployees) {
+        employeeData.push({
+            name: employeeRaw,
+            personalNum: employeeRaw.length
+        })
+    }
+
+    employeeData.forEach((employee) => console.log(`Name: ${employee.name} -- Personal Number: ${employee.personalNum}`));
+}
