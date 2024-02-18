f1e9f8ba0a4d752e5ed0b708c058207ecfb434d5
Simeon <simeonsavov21@gmail.com>
2024-02-18 21:34:04 +0200

    feat: Enhance code coverage for C# Library class with unit tests
    Improved code coverage for the C# Library class by adding comprehensive unit tests, ensuring robustness and reliability of the library management system.
    
    The Library class manages a collection of books and provides methods for adding, checking out, returning, and retrieving all books. Unit tests were written in the LibraryTests class to thoroughly validate the functionality of these methods under various conditions.
    
    This commit strengthens the test suite, covering edge cases such as adding new books, checking out and returning books, and handling scenarios where books do not exist or are already checked out. The enhanced code coverage ensures the integrity of the Library class, promoting quality assurance in the library management system.

---
+++ 15.Code-Coverage/library-managament/LibraryManagementTest/LibraryTests.cs
@@ -0,0 +1,169 @@
+using LibraryManagementSystem;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementTest;
+
+[TestFixture]
+public class LibraryTests
+{
+    [Test]
+    public void AddBook_ShouldAddANewBookToTheLibrary()
+    {
+        // Arrange
+        Library library = new Library();
+        var newBook = new Book
+        {
+            Author = "Simeon",
+            Id = 1,
+            IsCheckedOut = false,
+            Title = "Title",
+        };
+
+        // Act
+        library.AddBook(newBook);
+
+        // Assert
+        var allBooks = library.GetAllBooks();
+        Assert.That(allBooks.Count(), Is.EqualTo(1));
+
+        var singleBook = allBooks.First();
+        Assert.That(singleBook.Author, Is.EqualTo(newBook.Author));
+        Assert.That(singleBook.Id, Is.EqualTo(newBook.Id));
+        Assert.IsFalse(singleBook.IsCheckedOut);
+        Assert.That(singleBook.Title, Is.EqualTo(newBook.Title));
+    }
+
+    [Test]
+    public void CheckOutBook_ShouldReturnFalse_IfBookDoesNotExist()
+    {
+        // Arrange
+        Library library = new Library();
+        var newBook = new Book
+        {
+            Author = "Simeon Savov",
+            Id = 1,
+            IsCheckedOut = false,
+            Title = "Title",
+        };
+        library.AddBook(newBook);
+
+        // Act
+        var result = library.CheckOutBook(11);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void CheckOutBook_ShouldReturnFalse_IfAlreadyCheckedOut()
+    {
+        // Arrange
+        Library library = new Library();
+        var newBook = new Book
+        {
+            Author = "Simeon Savov",
+            Id = 1,
+            IsCheckedOut = true,
+            Title = "Title",
+        };
+        library.AddBook(newBook);
+
+        // Act
+        var result = library.CheckOutBook(newBook.Id);
+
+        // Assert
+        Assert.IsFalse(result);
+
+    }
+
+    [Test]
+    public void CheckOutBook_ShouldReturnTrue_IfWeCanCheckOutTheBook()
+    {
+        // Arrange
+        Library library = new Library();
+        var newBook = new Book
+        {
+            Author = "Simeon Savov",
+            Id = 1,
+            IsCheckedOut = false,
+            Title = "Title",
+        };
+        library.AddBook(newBook);
+
+        // Act
+        var result = library.CheckOutBook(newBook.Id);
+
+        // Assert
+        Assert.IsTrue(result);
+        var allBooks = library.GetAllBooks();
+        var singleBook = allBooks.First();
+        Assert.IsTrue(singleBook.IsCheckedOut);
+    }
+
+    [Test]
+    public void ReturnBook_ShouldReturnFalse_IfBookDoesNotExist()
+    {
+        // Arrange
+        Library library = new Library();
+        var newBook = new Book
+        {
+            Author = "Simeon Savov",
+            Id = 1,
+            IsCheckedOut = false,
+            Title = "Title",
+        };
+        library.AddBook(newBook);
+
+        // Act
+        var result = library.ReturnBook(30);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void ReturnBook_ShouldReturnFalse_IfBookIsNotCheckedOut()
+    {
+        // Arrange
+        Library library = new Library();
+        var newBook = new Book
+        {
+            Author = "Simeon Savov",
+            Id = 1,
+            IsCheckedOut = false,
+            Title = "Title First",
+        };
+        library.AddBook(newBook);
+
+        // Act
+        var result = library.ReturnBook(1);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void ReturnBook_ShouldReturnTrue_IfBookCanBeCheckedOut()
+    {
+        // Arrange
+        Library library = new Library();
+        var newBook = new Book
+        {
+            Author = "Simeon Savov",
+            Id = 1,
+            IsCheckedOut = true,
+            Title = "Title First",
+        };
+        library.AddBook(newBook);
+
+        // Act
+        var result = library.ReturnBook(1);
+
+        // Assert
+        Assert.IsTrue(result);
+        var allBooks = library.GetAllBooks();
+        var singleBook = allBooks.First();
+        Assert.IsFalse(singleBook.IsCheckedOut);
+    }
+}
+
